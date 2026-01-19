be12379bccc46721204d07bd3b6a32fc668d81eb
DevendraHolikar <devendraholikar@gmail.com>
2026-01-19 17:21:42 +0530

    button text fixed

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -7,27 +7,32 @@ const Products = () => {
   const { products, loading, error } = useProductFetch(PRODUCTS_API);
   const [userInput, setUserInput] = useState("");
   const [filteredData, setFilteredData] = useState();
+  const [searchButtonName, setSearchButtonName] = useState("Search");
 
   useEffect(() => {
     setFilteredData(products);
   }, [products]);
 
   const handleSearchClick = () => {
-
-    const filteredItems = products.filter((item) =>{
-      return item.title.toLowerCase().includes(userInput.toLowerCase())
+    const filteredItems = products.filter((item) => {
+      return item.title.toLowerCase().includes(userInput.toLowerCase());
     });
     setFilteredData(filteredItems);
+    setSearchButtonName("Search");
   };
 
   const handleTopRated = () => {
-    const topRatedFiltered = products.filter((item) => { return item.rating >= 4}  );
+    const topRatedFiltered = products.filter((item) => {
+      return item.rating >= 4;
+    });
     setFilteredData(topRatedFiltered);
+    setSearchButtonName("All");
   };
 
-  if (loading) {
-    return <h2 className="text-center text-white mt-2">Loading products...</h2>;
-  }
+  // if (loading) {
+  //   return <h2 className="text-center text-white mt-2">Loading products...</h2>;
+  // }
+
   if (error) {
     return <h2 className="text-center text-red-500">Error: {error}</h2>;
   }
@@ -43,25 +48,31 @@ const Products = () => {
           className="input"
         />
         <button onClick={handleSearchClick} className="btn">
-          Search
+          {searchButtonName}
         </button>
         <button onClick={handleTopRated} className="btn">
           Top Rated
         </button>
       </div>
-     { (filteredData.length === 0 ) ? <h2 className="text-center text-white mt-2">No Results Found</h2> : <div className="flex flex-wrap justify-items-start gap-6 mt-6 m-auto">
-        {filteredData.map((item) => (
-          <ProductCard
-            key={item.id}
-            id={item.id}
-            title={item.title}
-            thumbnail={item.thumbnail}
-            description={item.description}
-            tags={item.tags}
-            rating={item.rating}
-          ></ProductCard>
-        ))}
-      </div>}
+      {loading ? (
+        <h2 className="text-center text-white mt-2">Loading products...</h2>
+      ) : filteredData.length === 0 ? (
+        <h2 className="text-center text-white mt-2">No Results Found</h2>
+      ) : (
+        <div className="flex flex-wrap justify-items-start gap-6 mt-6 m-auto">
+          {filteredData.map((item) => (
+            <ProductCard
+              key={item.id}
+              id={item.id}
+              title={item.title}
+              thumbnail={item.thumbnail}
+              description={item.description}
+              tags={item.tags}
+              rating={item.rating}
+            ></ProductCard>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
